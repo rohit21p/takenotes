4be4abd54c9b622e91bc3c4c58bed8719f18c2a2
Rohit <iamrohit.panjwani@gmail.com>
2019-11-10 01:05:37 +0530

    note is saved properly

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-note',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNoteComponent implements OnInit {
 
-  constructor() { }
+  title;
+  desc;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+
+  create() {
+    this.http.post('http://localhost:3000/create', {
+     title: this.title,
+     desc: this.desc 
+    }, {
+      withCredentials: true
+    }).subscribe(data=> {
+      console.log(data);
+    })
   }
 
 }
